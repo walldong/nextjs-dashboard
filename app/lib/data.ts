8c8c494615211d957544ecdeafebb27467706828
欧阳会东 <ouyanghd@glodon.com>
2025-05-09 14:36:28 +0800

    feat(dashboard): refactor RevenueChart to fetch data internally and improve loading state
    - Updated RevenueChart component to fetch revenue data directly using fetchRevenue function.
    - Added loading skeletons for dashboard components to enhance user experience during data fetching.
    - Created a new loading component for the dashboard overview.
    - Implemented suspense fallback for RevenueChart and LatestInvoices to display skeletons while loading.
    - Updated pnpm-lock.yaml to reflect dependency updates, including sharp and next packages.
    - Introduced pnpm-workspace.yaml to ignore built dependencies for sharp.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,12 +16,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -32,6 +32,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -61,7 +62,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
-
+    await new Promise((resolve) => setTimeout(resolve, 5000));
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
